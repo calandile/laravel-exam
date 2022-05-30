f87af0c8f7a7c8ee9747f123a9756aa9331f0629
calandile <calandile@gmail.com>
2022-05-30 18:42:47 +0200

    Layout changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.default')
 @section('content')
     <section>
-        <div class="py-96">
-            <form method="POST" action="/result" class="flex items-center">
+        <div class="mt-96 flex justify-center">
+            <form method="POST" action="/result" class="flex flex-col items-center">
                 @csrf
-                <label for="simple-search" class="sr-only">Title</label>
-                <div class="relative w-full">
+                <div class="relative">
                     <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                         <svg class="h-5 w-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg">
@@ -14,17 +13,22 @@
                                 clip-rule="evenodd"></path>
                         </svg>
                     </div>
-                    <input type="text" id="simple-search" name="title"
-                        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                        placeholder="Search" required="">
+                    <input type="text" name="title"
+                        class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-pink-400 focus:ring-pink-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                        placeholder="Movie title" required="">
                 </div>
-                <button type="submit"
-                    class="ml-2 rounded-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg
-                        class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                    </svg></button>
+                <div class="mt-4 flex">
+                    <a href="/surprise"><button type="button"
+                            class="ml-2 rounded-lg bg-pink-500 bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 px-5 py-2.5 text-center text-sm font-medium text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 focus:outline-none focus:ring-4 focus:ring-pink-300 dark:focus:ring-pink-800">Surpise
+                            me!</button></a>
+                    <button type="submit"
+                        class="ml-2 rounded-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-br focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg
+                            class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                        </svg></button>
+                </div>
             </form>
         </div>
     </section>
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -2,26 +2,38 @@
 @section('content')
     <section>
         <a href="/">Back</a>
-        <div class="flex flex-wrap">
+        <div class="flex flex-col">
             @foreach ($response as $result)
                 <div
-                    class="max-w-sm rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800">
+                    class="mt-4 flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 md:max-w-fit">
                     <img class="rounded-t-lg" src="{{ $result['Poster'] }}" alt="" />
                     <div class="p-5">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        <h5
+                            class="mb-2 flex justify-between text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {{ $result['Title'] }}
+                            @if (Auth::check())
+                                <form method="POST" action="/result/favorite">
+                                    @csrf
+                                    <input type="hidden" name="title" value="{{ $result['Title'] }}">
+                                    <button type="submit"
+                                        class="ml-2 rounded-lg border border-blue-700 bg-blue-700 p-2.5 text-sm font-medium text-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-br focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg
+                                            class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                        </svg></button>
+                                </form>
+                            @else
+                                <a href="/login"><i class="fa-solid fa-heart"></i></a>
+                            @endif
                         </h5>
                         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $result['Year'] }}</p>
-                        <a href="#"
-                            class="inline-flex items-center rounded-lg bg-blue-700 py-2 px-3 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Read more
-                            <svg class="ml-2 -mr-1 h-4 w-4" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
+                        <p>Curabitur tempor feugiat erat nec imperdiet. Nunc lacinia felis eget odio vehicula congue.
+                            Suspendisse volutpat justo a rhoncus gravida. Sed mauris erat, ultrices volutpat viverra
+                            consectetur, placerat et tortor. Donec eget porttitor enim. Aliquam eget augue urna. Praesent
+                            fermentum ultrices varius. Maecenas fermentum nibh ac quam iaculis mollis. Suspendisse commodo
+                            neque sem, sed rhoncus nisi hendrerit non. Phasellus eget nisi sit amet ipsum rhoncus convallis
+                            in at odio. Mauris ac mi gravida, tincidunt sem eget, venenatis erat.</p>
                     </div>
                 </div>
             @endforeach
